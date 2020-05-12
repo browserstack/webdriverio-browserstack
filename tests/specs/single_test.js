c4dca1f157c696dc8f3790d4f391c1c2142b9f59
Anthony Fernandes <afdes25@gmail.com>
2020-05-13 04:31:18 +0530

    Clicking the Enter button instead of Search button

--- tests/specs/single_test.js
+++ tests/specs/single_test.js
@@ -2,7 +2,7 @@ describe('Google\'s Search Functionality', () => {
   it('can find search results', () => {
       browser.url('https://www.google.com/ncr');
       $('[name="q"]').setValue('BrowserStack');
-      $('[name="btnK"]').click();
+      browser.keys("\uE007");
       browser.getTitle().should.match(/BrowserStack - Google Search/i);
   });
 });
